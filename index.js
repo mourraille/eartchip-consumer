f10ec095668a74f636bb77a5f56b10f20df58dfc
Mauricio Mourraille <mourraille@me.com>
2021-10-19 15:42:32 +0200

    express functionality added  Please enter the commit message for your changes. Lines starting

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 import { connect } from 'mqtt'
 import dotenv from 'dotenv'
+import express from 'express'
+const app = express()
 dotenv.config()
 
+
 var client  = connect(process.env.MQTT_HOST)
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+  
+  app.listen(process.env.EXPRESS_PORT, () => {
+      console.log(`Earthchip v${process.env.APP_V} |  ${new Date()}`)
+      console.log(`App listening at ${process.env.EXPRESS_URL}:${process.env.EXPRESS_PORT}`)
+  })
+
 
 client.on('connect', function () {
   client.subscribe(['init', 'temp', 'soil', 'hum'], function (err) {
     if (!err) {
-      client.publish('init', "Earthchip v" + process.env.APP_V + " | "+ new Date() )
+        client.publish('init', `MQTT CONNECTION STABLISHED TO ${process.env.MQTT_HOST} `)
+    } else  {
+        console.log(`MQTT CONNECTION STABLISHED TO ${process.env.MQTT_HOST}`)
     }
-  })
+})
 })
 
 client.on('message', function (topic, message) {
